90b2707193b3493c2ab21f1dabaeb7960eec465f
zi27281 <zi27282@seeu.edu.mk>
2019-10-20 23:09:33 +0200

    assignment 1

--- Week II/SK.StudentTranscript/SK.StudentTranscript/Program.cs
+++ Week II/SK.StudentTranscript/SK.StudentTranscript/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace SK.StudentTranscript
+namespace StudentTranscript.Update
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] courses = new string[20];
-            int[] grades = new int[20];
-            int[] credits = new int[20];
-            int currentIndex = 0;
+            List<string> courses = new List<string>();
+            List<int> credits = new List<int>();
+            List<int> grades = new List<int>();
+            
             while (true)
             {
                 Console.WriteLine("Ju lutem shkruani emrin e lendes, ose shkruani X per te vazhduar!");
@@ -17,41 +18,79 @@ namespace SK.StudentTranscript
                 if (courseName == "X" || courseName == "x")
                     break;
 
-                courses[currentIndex] = courseName;
+                courses.Add(courseName);
 
-                Console.WriteLine("Ju lutem shkruani numrin e kredive per kete lende!");
-                int credit = Convert.ToInt32(Console.ReadLine());
-                credits[currentIndex] = credit;
+                int grade, credit;
+                Console.WriteLine("Ju lutem shkruani noten per kete lende:  " + courseName);
+                string stringGrade = Console.ReadLine().ToLower();
 
-                Console.WriteLine("Ju lutem shkruani noten per kete lende!");
-                int grade = Convert.ToInt32(Console.ReadLine());
-                grades[currentIndex] = grade;
-                currentIndex++;
+                if (stringGrade == "in") stringGrade = "-1";
+                else if (stringGrade == "nr") stringGrade = "-2";
+                
+                grade = Convert.ToInt32(stringGrade);
+                
+                while ((grade != -2 && grade != -1) && (grade < 5 || grade > 10))
+                {
+                    Console.WriteLine("Notat duhet te jene te rangut 5-10, IN ose NR: " + courseName);
+                    stringGrade = Console.ReadLine().ToLower();
+                    
+                    if (stringGrade == "in") stringGrade = "-1";
+                    else if (stringGrade == "nr") stringGrade = "-2";
+                    
+                    grade = Convert.ToInt32(stringGrade);
+                }
+
+                Console.WriteLine("Ju lutem shkruani kreditet per kete lende: " + courseName);
+                credit = Convert.ToInt32(Console.ReadLine());
+                while (credit < 1 || credit > 10)
+                {
+                    Console.WriteLine("Kreditet duhet te jete nga numri 1 deri ne 10: " + courseName);
+                    credit = Convert.ToInt32(Console.ReadLine());
+                }
+
+                credits.Add(credit);
+                grades.Add(grade);
             }
-            
+
             Console.WriteLine("        TRANSKRIPTA E STUDENTIT");
             Console.WriteLine("=======================================");
-            Console.WriteLine("| LENDA \t\t KREDITE \t NOTA");
+            Console.WriteLine("| LENDA \t KREDITE \t NOTA");
             Console.WriteLine("=======================================");
             double shumaKredive = 0;
+            double totalKredit = 0;
             double shumaNotave = 0;
-            for (var i = 0; i<currentIndex; i++)
-            {
-                Console.WriteLine(courses[i] + "\t\t" + credits[i] + "\t" + grades[i]);
-                shumaKredive += credits[i];
-                shumaNotave += credits[i] * grades[i];
-            }
-            double gpa = shumaNotave / shumaKredive;
 
-            Console.WriteLine("=======================================");
-            Console.WriteLine("Nota mesatare: " + gpa.ToString("0.00"));
-            Console.ReadLine();
+            for (var i = 0; i < courses.Count; i++)
+            {
+                Console.WriteLine(courses[i] + "\t\t" + credits[i] + "\t\t" + GradeString(grades[i]));
+
+                totalKredit += credits[i];
+
+                int grade = grades[i];
+                if (grade != -2 && grade != -1 && grade != 5)
+                {
+                    shumaKredive += credits[i];
+                    shumaNotave += credits[i] * grades[i];
+                }
+            }
+
+            double gpa = 0;
+
+            if (shumaKredive != 0) gpa = shumaNotave / shumaKredive;
+
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Credits attempted: " + totalKredit);
+            Console.WriteLine("Credits awarded: " + shumaKredive);
+            Console.WriteLine("Number of courses: " + courses.Count);
+            Console.WriteLine("GPA: " + gpa.ToString("0.00"));
+        }
+
+        private static string GradeString(int grade)
+        {
+            if (grade == -1) return "IN";
+            else if (grade == -2) return "NR";
+
+            return grade.ToString();
         }
     }
 }
-
-
-
-
-
-
