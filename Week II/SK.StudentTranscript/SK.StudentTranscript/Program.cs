bcfa42e444c0d9d4f677327c73913b5a390457df
Kaltrina127316 <56677443+Kaltrina127316@users.noreply.github.com>
2019-10-23 16:02:10 +0200

    Update Program.cs

--- Week II/SK.StudentTranscript/SK.StudentTranscript/Program.cs
+++ Week II/SK.StudentTranscript/SK.StudentTranscript/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-
+using System;
+using System.Collections.Generic;
 namespace SK.StudentTranscript
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] courses = new string[20];
-            int[] grades = new int[20];
-            int[] credits = new int[20];
-            int currentIndex = 0;
+            List<String> courses = new List<String>();
+            List<int> grades = new List<int>();
+            List<int> credits = new List<int>();
+          
             while (true)
             {
                 Console.WriteLine("Ju lutem shkruani emrin e lendes, ose shkruani X per te vazhduar!");
@@ -17,34 +17,76 @@ namespace SK.StudentTranscript
                 if (courseName == "X" || courseName == "x")
                     break;
 
-                courses[currentIndex] = courseName;
+                //courses[currentIndex] = courseName;
+                courses.Add(courseName);
 
-                Console.WriteLine("Ju lutem shkruani numrin e kredive per kete lende!");
-                int credit = Convert.ToInt32(Console.ReadLine());
-                credits[currentIndex] = credit;
+                while (true)
+                {
+                    Console.WriteLine("Ju lutem shkruani numrin e kredive per kete lende!");
+                    int credit = Convert.ToInt32(Console.ReadLine());
+                    if (credit >= 1 && credit <= 10)
+                    {
+                        credits.Add(credit);
+                        break;
+                    }
+                    
+                   
+                }
 
-                Console.WriteLine("Ju lutem shkruani noten per kete lende!");
-                int grade = Convert.ToInt32(Console.ReadLine());
-                grades[currentIndex] = grade;
-                currentIndex++;
+
+
+
+                while (true)
+                {
+                    Console.WriteLine("Ju lutem shkruani noten per kete lende! \n Shkruani -1 per IN ose -2 per NR");
+                    int grade = Convert.ToInt32(Console.ReadLine());
+
+                    if ((grade >= 5 && grade <= 10) || grade == -1 || grade == -2)
+                    {
+                        grades.Add(grade);
+                        break;
+                        }
+                    
+
+                }
+
             }
             
+         
+            Console.WriteLine(courses[0] + " "+ grades[0]);
             Console.WriteLine("        TRANSKRIPTA E STUDENTIT");
             Console.WriteLine("=======================================");
             Console.WriteLine("| LENDA \t\t KREDITE \t NOTA");
             Console.WriteLine("=======================================");
             double shumaKredive = 0;
             double shumaNotave = 0;
-            for (var i = 0; i<currentIndex; i++)
+            double shumaKredivePassed = 0;
+            for (var i = 0; i<courses.Count; i++)
             {
-                Console.WriteLine(courses[i] + "\t\t" + credits[i] + "\t" + grades[i]);
-                shumaKredive += credits[i];
-                shumaNotave += credits[i] * grades[i];
-            }
-            double gpa = shumaNotave / shumaKredive;
+                Console.Write(courses[i] + "\t\t" + credits[i] + "\t\t");
 
+                if (grades[i] == -1)
+                    Console.Write("IN");
+                else if (grades[i] == -2)
+                    Console.Write("NR");
+                else
+                    Console.Write(grades[i]);
+
+                Console.WriteLine("");
+                shumaKredive += credits[i];
+                if (grades[i] > 5)
+                {
+                    shumaKredivePassed += credits[i];
+                    shumaNotave += credits[i] * grades[i];
+                }
+            }
+            double gpa = shumaNotave / shumaKredivePassed;
+            
             Console.WriteLine("=======================================");
             Console.WriteLine("Nota mesatare: " + gpa.ToString("0.00"));
+            Console.WriteLine("Credits Attempted: " + shumaKredive);
+            Console.WriteLine("Credits Awarded: " + shumaKredivePassed);
+            Console.WriteLine("Number of courses: " + courses.Count);
             Console.ReadLine();
         }
     }
