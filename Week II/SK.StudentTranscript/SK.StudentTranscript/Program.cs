f725935927ff792cbbd7833a9151c593d77f20c4
omerxhaferi <40522999+omerxhaferi@users.noreply.github.com>
2019-10-24 10:05:11 +0200

    Homework

--- Week II/SK.StudentTranscript/SK.StudentTranscript/Program.cs
+++ Week II/SK.StudentTranscript/SK.StudentTranscript/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SK.StudentTranscript
 {
@@ -6,45 +7,90 @@ namespace SK.StudentTranscript
     {
         static void Main(string[] args)
         {
-            string[] courses = new string[20];
-            int[] grades = new int[20];
-            int[] credits = new int[20];
+
+            List<string> courses = new List<string>();
+            List<int> grades = new List<int>();
+            List<int> allGrades = new List<int>();
+
+            List<int> creditsAttempted = new List<int>();
+            int creditsAwarded = 0;
+
             int currentIndex = 0;
+            double shumaNotave = 0;
+            int creditIndex = 0;
+
             while (true)
             {
-                Console.WriteLine("Ju lutem shkruani emrin e lendes, ose shkruani X per te vazhduar!");
+                Console.WriteLine("\nJu lutem shkruani emrin e lendes, ose shkruani X per te mbyllur!");
                 string courseName = Console.ReadLine();
                 if (courseName == "X" || courseName == "x")
                     break;
 
-                courses[currentIndex] = courseName;
+                courses.Add(courseName);
 
-                Console.WriteLine("Ju lutem shkruani numrin e kredive per kete lende!");
+                Console.WriteLine("Ju lutem shkruani numrin e kredive per kete lende! [1 - 10]");
                 int credit = Convert.ToInt32(Console.ReadLine());
-                credits[currentIndex] = credit;
 
-                Console.WriteLine("Ju lutem shkruani noten per kete lende!");
+
+                while (credit < 1 || credit > 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Shifer e gabuar. Provoni perseri.\nJu lutem shkruani numrin e kredive per kete lende! [1 - 10]");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    credit = Convert.ToInt32(Console.ReadLine());
+                }
+
+                creditsAttempted.Add(credit);
+
+                Console.WriteLine("Ju lutem shkruani noten per kete lende! [5 - 10] | Shenoni 11 per IN dhe 12 per NR");
                 int grade = Convert.ToInt32(Console.ReadLine());
-                grades[currentIndex] = grade;
+
+
+                while (grade < 5 || grade > 12)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Shifer e gabuar. Provoni perseri.\nJu lutem shkruani noten per kete lende! [5 - 10] | Shenoni -1 per IN dhe -2 per NR");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    grade = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+                allGrades.Add(grade);
+
+                if (grade > 5 && grade < 11 && grade != 11 && grade != 12) 
+                {
+                    grades.Add(grade);
+                    creditsAwarded += credit;
+
+                    shumaNotave += credit * grades[creditIndex];
+                    creditIndex++;
+                }
+
                 currentIndex++;
             }
-            
-            Console.WriteLine("        TRANSKRIPTA E STUDENTIT");
+
+            Console.WriteLine("\n\n        TRANSKRIPTA E STUDENTIT");
             Console.WriteLine("=======================================");
-            Console.WriteLine("| LENDA \t\t KREDITE \t NOTA");
+            Console.WriteLine("\tLENDA\tKREDITE\tNOTA");
             Console.WriteLine("=======================================");
-            double shumaKredive = 0;
-            double shumaNotave = 0;
-            for (var i = 0; i<currentIndex; i++)
+
+            double shumaKrediveAttempted = 0;
+
+
+            for (var i = 0; i < currentIndex; i++)
             {
-                Console.WriteLine(courses[i] + "\t\t" + credits[i] + "\t" + grades[i]);
-                shumaKredive += credits[i];
-                shumaNotave += credits[i] * grades[i];
+                Console.WriteLine("\t" + courses[i] + "\t" + creditsAttempted[i] + "\t" + allGrades[i].ToString().Replace("11", "IN").Replace("12", "NR"));
+                shumaKrediveAttempted += creditsAttempted[i];
             }
-            double gpa = shumaNotave / shumaKredive;
+
+            double gpa = shumaNotave / creditsAwarded;
 
             Console.WriteLine("=======================================");
-            Console.WriteLine("Nota mesatare: " + gpa.ToString("0.00"));
+            Console.WriteLine("Number of courses: " + currentIndex.ToString());
+            Console.WriteLine("Credites attempted: " + shumaKrediveAttempted.ToString());
+            Console.WriteLine("Credites awarded: " + creditsAwarded.ToString());
+
+            Console.WriteLine("GPA: " + gpa.ToString("0.00"));
             Console.ReadLine();
         }
     }
